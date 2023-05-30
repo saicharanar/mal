7613da24a00fed8f0c1e73f9ebe61d28cbe255d5
Sai Charan Abbireddy <99522580+saicharanar@users.noreply.github.com>
2023-05-30 11:36:49 +0530

    Implemented step 3.

--- impls/cherry/step3_env.js
+++ impls/cherry/step3_env.js
@@ -1,7 +1,8 @@
 const readline = require('readline');
 const { read_str } = require('./reader.js');
 const { pr_str } = require('./printer.js');
-const { MalSymbol, MalList, MalValue, MalVector, MalObject } = require('./types.js');
+const { MalSymbol, MalList, MalValue, MalVector, MalObject, MalNil } = require('./types.js');
+const { Env } = require('./env.js');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -13,16 +14,10 @@ const mul = (x, y) => new MalValue(x.value * y.value);
 const sub = (x, y) => new MalValue(x.value - y.value);
 const div = (x, y) => new MalValue(x.value / y.value);
 
-const env = {
-  '+': (...args) => args.reduce(sum),
-  '*': (...args) => args.reduce(mul),
-  '-': (...args) => args.reduce(sub),
-  '/': (...args) => args.reduce(div),
-};
 
 const eval_ast = (ast, env) => {
   if (ast instanceof MalSymbol) {
-    return env[ast.value]
+    return env.get(ast);
   }
 
   if (ast instanceof MalList) {
@@ -38,6 +33,26 @@ const eval_ast = (ast, env) => {
   return ast;
 };
 
+const bindLet = (ast, env) => {
+  const scopeEnv = new Env(env);
+  const bindings = ast.value[1].value;
+
+  for (let i = 0; i < bindings.length; i += 2) {
+    scopeEnv.set(bindings[i], EVAL(bindings[i + 1], scopeEnv));
+  }
+
+  const exp = ast.value[2];
+  if (exp) {
+    return EVAL(exp, scopeEnv);
+  }
+
+  return new MalNil();
+};
+
+const bindDef = (ast, env) => {
+  env.set(ast.value[1], EVAL(ast.value[2], env));
+  return env.get(ast.value[1]);
+};
 
 const EVAL = (ast, env) => {
   if (!(ast instanceof MalList)) {
@@ -46,12 +61,29 @@ const EVAL = (ast, env) => {
 
   if (ast.isEmpty()) return ast;
 
+  switch (ast.value[0].value) {
+    case 'def!':
+      return bindDef(ast, env);
+
+    case 'let*':
+      return bindLet(ast, env);
+
+  }
+
   const [fn, ...args] = eval_ast(ast, env).value;
   return fn.apply(null, args);
 };
 
+
 const READ = (str) => read_str(str);
 const PRINT = (malValue) => pr_str(malValue);
+
+const env = new Env();
+env.set(new MalSymbol('+'), (...args) => args.reduce(sum))
+env.set(new MalSymbol('-'), (...args) => args.reduce(sub))
+env.set(new MalSymbol('*'), (...args) => args.reduce(mul))
+env.set(new MalSymbol('/'), (...args) => args.reduce(div))
+
 const rep = (str) => PRINT(EVAL(READ(str), env));
 
 const repl = () => {
