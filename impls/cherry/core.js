7e239bebc5c126771633f5b790c1d3a04b677ed5
Sai Charan Abbireddy <99522580+saicharanar@users.noreply.github.com>
2023-06-01 14:17:40 +0530

    Refactored the code into core.js

---
+++ impls/cherry/core.js
@@ -0,0 +1,74 @@
+const { Env } = require('./env.js');
+const { isDeepStrictEqual } = require('util');
+const { MalSymbol, MalList, MalValue, MalVector, MalObject, MalNil } = require('./types.js');
+const { pr_str } = require('./printer.js');
+
+const createList = (...args) => {
+  return new MalList(args);
+};
+
+const isList = (arg) => {
+  return (arg instanceof MalList);
+};
+
+const isEmpty = (arg) => {
+  return arg.isEmpty();
+};
+
+const count = (arg) => {
+  if (arg instanceof MalNil) {
+    return new MalValue(0);
+  }
+
+  return new MalValue(arg.count());
+};
+
+const prn = (...args) => {
+  console.log(...args.map(item => pr_str(item)));
+  return new MalNil();
+};
+
+const binOperator = (predicate) => (...args) => {
+  for (let i = 1; i < args.length; i++) {
+    console.log(args[i]);
+    const RHS = args[i - 1].value;
+    const LHS = args[i].value;
+
+    if (!predicate(RHS, LHS)) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const equals = (a, b) => isDeepStrictEqual(a, b);
+const lessThan = (a, b) => a < b;
+const greaterThan = (a, b) => a > b;
+const lessThanEqual = (a, b) => a <= b;
+const greaterThanEqual = (a, b) => a >= b;
+const sum = (x, y) => new MalValue(x.value + y.value);
+const mul = (x, y) => new MalValue(x.value * y.value);
+const sub = (x, y) => new MalValue(x.value - y.value);
+const div = (x, y) => new MalValue(x.value / y.value);
+
+
+
+const env = new Env();
+env.set(new MalSymbol('+'), (...args) => args.reduce(sum));
+env.set(new MalSymbol('-'), (...args) => args.reduce(sub));
+env.set(new MalSymbol('*'), (...args) => args.reduce(mul));
+env.set(new MalSymbol('/'), (...args) => args.reduce(div));
+env.set(new MalSymbol('list'), createList);
+env.set(new MalSymbol('list?'), isList);
+env.set(new MalSymbol('empty?'), isEmpty);
+env.set(new MalSymbol('count'), count);
+env.set(new MalSymbol('prn'), prn);
+env.set(new MalSymbol('println'), prn);
+env.set(new MalSymbol('='), binOperator(equals));
+env.set(new MalSymbol('<'), binOperator(lessThan));
+env.set(new MalSymbol('<='), binOperator(lessThanEqual));
+env.set(new MalSymbol('>'), binOperator(greaterThan));
+env.set(new MalSymbol('>='), binOperator(greaterThanEqual));
+
+module.exports = { env };
--- impls/cherry/step4_if_fn_do.js
+++ impls/cherry/step4_if_fn_do.js
@@ -2,19 +2,13 @@ const readline = require('readline');
 const { read_str } = require('./reader.js');
 const { pr_str } = require('./printer.js');
 const { MalSymbol, MalList, MalValue, MalVector, MalObject, MalNil } = require('./types.js');
+const { env } = require('./core.js');
 const { Env } = require('./env.js');
-const { isDeepStrictEqual } = require('util')
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
-const sum = (x, y) => new MalValue(x.value + y.value);
-const mul = (x, y) => new MalValue(x.value * y.value);
-const sub = (x, y) => new MalValue(x.value - y.value);
-const div = (x, y) => new MalValue(x.value / y.value);
-
 
 const eval_ast = (ast, env) => {
   if (ast instanceof MalSymbol) {
@@ -69,14 +63,6 @@ const doBlock = (ast, env) => {
   return (EVAL(...lists.slice(-1), env));
 };
 
-const createList = (...args) => {
-  return new MalList(args);
-};
-
-const isList = (arg) => {
-  return (arg instanceof MalList);
-};
-
 const ifBlock = (ast, env) => {
   const [predicate, ifBlock, elsePart] = ast.value.slice(1);
 
@@ -89,23 +75,6 @@ const ifBlock = (ast, env) => {
     return EVAL(elsePart, env);
   }
 
-  return new MalNil();
-};
-
-const isEmpty = (arg) => {
-  return arg.isEmpty();
-};
-
-const count = (arg) => {
-  if (arg instanceof MalNil) {
-    return new MalValue(0);
-  }
-
-  return new MalValue(arg.count());
-};
-
-const prn = (...args) => {
-  console.log(...args.map(item => pr_str(item)));
   return new MalNil();
 };
 
@@ -147,47 +116,9 @@ const EVAL = (ast, env) => {
   return fn.apply(null, args);
 };
 
-const binOperator = (predicate) => (...args) => {
-  for (let i = 1; i < args.length; i++) {
-    console.log(args[i]);
-    const RHS = args[i - 1].value;
-    const LHS = args[i].value;
-
-    if (!predicate(RHS, LHS)) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-
-const equals = (a, b) => isDeepStrictEqual(a, b);
-const lessThan = (a, b) => a < b;
-const greaterThan = (a, b) => a > b;
-const lessThanEqual = (a, b) => a <= b;
-const greaterThanEqual = (a, b) => a >= b;
 
 const READ = (str) => read_str(str);
 const PRINT = (malValue) => pr_str(malValue);
-
-const env = new Env();
-env.set(new MalSymbol('+'), (...args) => args.reduce(sum));
-env.set(new MalSymbol('-'), (...args) => args.reduce(sub));
-env.set(new MalSymbol('*'), (...args) => args.reduce(mul));
-env.set(new MalSymbol('/'), (...args) => args.reduce(div));
-env.set(new MalSymbol('list'), createList);
-env.set(new MalSymbol('list?'), isList);
-env.set(new MalSymbol('empty?'), isEmpty);
-env.set(new MalSymbol('count'), count);
-env.set(new MalSymbol('prn'), prn);
-env.set(new MalSymbol('println'), prn);
-env.set(new MalSymbol('='), binOperator(equals));
-env.set(new MalSymbol('<'), binOperator(lessThan));
-env.set(new MalSymbol('<='), binOperator(lessThanEqual));
-env.set(new MalSymbol('>'), binOperator(greaterThan));
-env.set(new MalSymbol('>='), binOperator(greaterThanEqual));
-
 const rep = (str) => PRINT(EVAL(READ(str), env));
 
 const repl = () => {
