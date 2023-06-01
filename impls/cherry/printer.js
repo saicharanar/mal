1db85569108ca3e613b65d361682a982408e07c5
Sai Charan Abbireddy <99522580+saicharanar@users.noreply.github.com>
2023-06-01 12:30:12 +0530

    fixed function return

--- impls/cherry/printer.js
+++ impls/cherry/printer.js
@@ -1,6 +1,10 @@
 const { MalValue } = require("./types");
 
 const pr_str = (malValue) => {
+  if (malValue instanceof Function) {
+    return '#<function>'
+  }
+
   if (malValue instanceof MalValue) {
     return malValue.pr_str();
   }
